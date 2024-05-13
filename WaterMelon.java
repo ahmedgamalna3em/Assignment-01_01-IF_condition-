87513bbf6d3e4d48646fd086ef7cf9203ed5892d
unknown <ahmedgamal56690@gmail.com>
2024-05-14 00:21:48 +0300

    Finish Task 6

--- WaterMelon.java
+++ WaterMelon.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class WaterMelon {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Write The Weight For WaterMelon : ");
+        int weight = input.nextInt() ;
+        if(weight%2==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
 
     }
 }
