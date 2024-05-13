b499ed81b68886d9cbbd11d6f37c9340f8472629
unknown <ahmedgamal56690@gmail.com>
2024-05-14 01:10:44 +0300

    Start Task 7

---
+++ SortingThreeFloatingNumber.java
@@ -0,0 +1,27 @@
+package First_Assignment_1_2;
+import java.util.Scanner;
+public class SortingThreeFloatingNumber {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        float num1,num2,num3;
+        System.out.print("Enter No.1 : ");
+        num1 = input.nextFloat();
+        System.out.print("Enter No.2 : ");
+        num2 = input.nextFloat();
+        System.out.print("Enter No.3 : ");
+        num3 = input.nextFloat();
+        if(num1==num2 && num2==num3){
+            System.out.println("The Three number is Equales.");
+        }else if (num1<num2 && num1<num3){
+            System.out.println(num1);
+        }
+        else if(num2<num3){
+            System.out.println(num2);
+            System.out.println(num3);
+        }
+
+
+
+
+    }
+}
